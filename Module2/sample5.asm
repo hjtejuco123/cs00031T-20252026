a7f756a7d3eed70c059473f7a4f083e8197ad15f
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-19 11:10:08 +0800

    Create sample5.asm

---
+++ Module2/sample5.asm
@@ -0,0 +1,32 @@
+
+;display ascii and hex characters
+;this is the data section
+section .data
+
+;msg db 65,66,67,10		;ascii char ABC
+msg db 0x41,32,0x42,32,0x43,0x0A ;hex
+msglen equ $ - msg 	;calculate the length 
+
+section .text
+global _start
+
+SYS_EXIT equ 1
+SYS_WRITE equ 4
+STDOUT equ 1
+
+_start:
+	;write system call (sys_write)
+	mov eax,SYS_WRITE	;sys_write
+	mov ebx,STDOUT		;stdout
+	mov ecx,msg 	;pointer to message
+	mov edx,msglen	;message length 
+	int 0x80		;invoke the syscall
+
+	;exit system call
+	mov eax, SYS_EXIT	;sys_exit 
+	xor ebx,ebx		;exit status 0
+	int 0x80		;invoke the syscall
+
+
+
+
