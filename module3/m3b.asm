05922bd568995b4715eee4bf711bce288ef56642
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-30 07:03:59 +0800

    Add files via upload

---
+++ module3/m3b.asm
@@ -0,0 +1,38 @@
+section .data
+    value   db 5
+    msg     db "Decremented value: "
+    msg_len equ $ - msg
+    digit   db 0
+    newline db 10
+
+section .text
+    global _start
+
+_start:
+    dec byte [value]
+
+    mov al, [value]
+    add al, '0'
+    mov [digit], al
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, msg_len
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, digit
+    mov edx, 1
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+
+    movzx ebx, byte [value]
+    mov eax, 1
+    int 0x80
