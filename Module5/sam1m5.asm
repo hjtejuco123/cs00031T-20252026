227d7b0fed7e9795eecc65ad17f55d68621a11c0
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-11-02 11:44:51 +0800

    Refactor output messages and clean up code

--- Module5/sam1m5.asm
+++ Module5/sam1m5.asm
@@ -1,42 +1,39 @@
+; sam1m5.asm
+; Minimal sample showing how to break simple output into procedures. The program
+; prints "Hello, World!" by calling two routines that wrap `sys_write`. Comments
+; describe the data layout and register/setup used for each call.
+
 section .data
     hello_message db 'Hello, '
     hello_length equ $ - hello_message
-    world_message db 'World!',0xA ; with newline character
+    world_message db 'World!', 10  ; include newline character
     world_length equ $ - world_message
+
 section .text
 ; Procedure to print "Hello, "
 print_hello:
-    ; Load the file descriptor (stdout) into ebx
-    mov ebx, 1
-    ; Load the address of hello_message into ecx
+    mov ebx, 1            ; stdout file descriptor
     mov ecx, hello_message
-    ; Load the length of the message into edx
     mov edx, hello_length
-    ; sys_write system call
-    mov eax, 4
+    mov eax, 4            ; sys_write
     int 0x80
     ret
-; Procedure to print "World!"
+
+; Procedure to print "World!\n"
 print_world:
-    ; Load the file descriptor (stdout) into ebx
-    mov ebx, 1
-    ; Load the address of world_message into ecx
+    mov ebx, 1            ; stdout file descriptor
     mov ecx, world_message
-    ; Load the length of the message into edx
     mov edx, world_length
-    ; sys_write system call
-    mov eax, 4
+    mov eax, 4            ; sys_write
     int 0x80
     ret
+
 ; Entry point of the program
 global _start
 _start:
-    ; Call the procedures
     call print_hello
     call print_world
-    ; Terminate the program using sys_exit
-    ; Load the exit status into ebx (0 for success)
-    mov ebx, 0
-    ; sys_exit system call
-    mov eax, 1
+
+    mov ebx, 0            ; exit status 0
+    mov eax, 1            ; sys_exit
     int 0x80
